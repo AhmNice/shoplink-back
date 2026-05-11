58ba91e39fed88625b68b28fd29aa2c991c19110
Musa Muhammed Awwal <talk2muhammedawwal@gmail.com>
2026-05-11 13:04:50 +0100

    fix sr

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app.js';
 import { config } from './config/config.js';
 import prisma from './db/database.js';
 
-const PORT = config.PORT;
+const PORT = process.env.PORT || config.PORT || 5000;
 
 const startServer = async () => {
   try {
