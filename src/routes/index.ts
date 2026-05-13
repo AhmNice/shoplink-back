9d557fc93f4cd02df577a4df11b9aa62733395f7
Musa Muhammed Awwal <talk2muhammedawwal@gmail.com>
2026-05-13 15:15:00 +0100

    add ping

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,11 @@ import productRouter from './product.route.js';
 import storeRouter from './store.route.js';
 
 const router = express.Router();
+
+router.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
 router.use('/auth', authRouter);
 router.use('/products', productRouter);
 router.use('/stores', storeRouter);
